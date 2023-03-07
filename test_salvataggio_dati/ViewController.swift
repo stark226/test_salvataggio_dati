2f7c23952f92113ad98fd86c7d12213f913f2766
Stefano <aedilis79@gmail.com>
2023-03-07 10:52:26 +0100

    Initial Commit

---
+++ test_salvataggio_dati/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_salvataggio_dati
+//
+//  Created by stefano cardia on 07/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
